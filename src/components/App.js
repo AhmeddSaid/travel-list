97281546897e55d1f9d3ec351d14fee7f1d36208
Ahmed Said <ahmedsaidadnan@outlook.com>
2023-07-04 14:21:41 +0300

    added local storage

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,56 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Logo from "./Logo";
 import Form from "./Form";
 import PackingList from "./PackingList";
 import Stats from "./Stats";
 
-let initialItems = [
+const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
   { id: 3, description: "Charger", quantity: 1, packed: false },
 ];
 
 function App() {
-  const [items, setItems] = useState(initialItems);
+  const [items, setItems] = useState([]);
 
-  // events
+  useEffect(() => {
+    console.log("Loading items from localStorage...");
+    const storedItems = localStorage.getItem("items");
+    if (storedItems) {
+      console.log("Found items in localStorage:", storedItems);
+      setItems((prevItems) => [...prevItems, ...JSON.parse(storedItems)]);
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log("Saving items to localStorage:", items);
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
+
+  // Events
   function handleAddItem(item) {
-    setItems((items) => [...items, item]);
+    console.log("Adding item:", item);
+    setItems((prevItems) => [...prevItems, item]);
   }
+
   function handleDeleteItem(id) {
-    setItems((items) => items.filter((item) => item.id !== id));
+    console.log("Deleting item with ID:", id);
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
   }
+
   function handleToggleItem(id) {
-    setItems((items) =>
-      items.map((item) => (item.id === id ? { ...item, packed: !item.packed } : item))
+    console.log("Toggling item with ID:", id);
+    setItems((prevItems) =>
+      prevItems.map((item) => (item.id === id ? { ...item, packed: !item.packed } : item))
     );
   }
+
   function handleClearList() {
-    const confirmed = window.confirm("are you sure you want to delete all items?");
-    confirmed && setItems([]);
+    const confirmed = window.confirm("Are you sure you want to delete all items?");
+    if (confirmed) {
+      console.log("Clearing all items.");
+      setItems([]);
+    }
   }
 
   return (
@@ -44,4 +67,5 @@ function App() {
     </div>
   );
 }
+
 export default App;
